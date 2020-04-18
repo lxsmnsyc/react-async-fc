bfc0f656d88f87a324bfa71910a14b48aadec915
Alexis Munsayac <alexis.munsayac@gmail.com>
2020-04-18 19:01:01 +0800

    Replace global caching with component-level caching

--- src/create-suspenseful.tsx
+++ src/create-suspenseful.tsx
@@ -1,85 +1,128 @@
 import React, { Suspense } from 'react';
-import { AsyncFCOptions, AsyncFunctionComponent, AsyncFCProps } from './types';
+import {
+  AsyncFCOptions, AsyncFunctionComponent, AsyncFCProps, AsyncFunctionComponentPropless,
+} from './types';
+import Subscription from './subscription';
+import NoCacheRefError from './utils/no-cache-ref-error';
+import compareDeps from './utils/compare-deps';
+import useIsomorphicEffect from './utils/useIsomorphicEffect';
 
 interface AsyncPending {
+  subscription: Subscription;
   value: Promise<void>;
   state: 'pending';
+  dependencies: React.DependencyList;
 }
 
 interface AsyncSuccess {
   value: JSX.Element;
   state: 'success';
+  dependencies: React.DependencyList;
 }
 
 interface AsyncFailure {
   value: any;
   state: 'failure';
+  dependencies: React.DependencyList;
 }
 
 type AsyncState = AsyncPending | AsyncSuccess | AsyncFailure;
 
+type RefType<T> = ((instance: T | null) => void) | React.MutableRefObject<T | null> | null;
+
+function isRefFunction<T>(ref: RefType<T>): ref is ((instance: T | null) => void) {
+  return typeof ref === 'function';
+}
 
 export default function createSuspenseful<P = {}>(
   render: AsyncFunctionComponent<P>,
-  { defaultFallback, keySupplier }: AsyncFCOptions<P>,
+  { defaultFallback, dependencies }: AsyncFCOptions<P>,
 ): React.FC<P & AsyncFCProps> {
-  const RESOURCES = new Map<string, AsyncState>();
-
   function load(
-    key: string,
-    provider: () => Promise<JSX.Element>,
+    cacheRef: RefType<AsyncState | null> | null,
+    provider: AsyncFunctionComponentPropless,
+    deps: React.DependencyList,
   ): JSX.Element {
-    let cachedResource = RESOURCES.get(key);
+    if (isRefFunction(cacheRef) || !cacheRef) {
+      throw new NoCacheRefError();
+    }
+    const shouldRefetch = compareDeps(deps, cacheRef.current?.dependencies);
 
-    if (!cachedResource) {
-      cachedResource = {
-        value: provider().then(
+    if (cacheRef.current == null || shouldRefetch) {
+      if (cacheRef.current?.state === 'pending') {
+        cacheRef.current.subscription.cancel();
+      }
+
+      const subscription = new Subscription();
+
+      cacheRef.current = {
+        state: 'pending',
+        subscription,
+        dependencies: deps,
+        value: provider(subscription).then(
           (value) => {
-            RESOURCES.set(key, {
-              value,
-              state: 'success',
-            });
+            if (!subscription.isCancelled) {
+              cacheRef.current = {
+                state: 'success',
+                value,
+                dependencies: deps,
+              };
+            }
           },
           (value) => {
-            RESOURCES.set(key, {
-              value,
-              state: 'failure',
-            });
+            if (!subscription.isCancelled) {
+              cacheRef.current = {
+                state: 'failure',
+                value,
+                dependencies: deps,
+              };
+            }
           },
         ),
-        state: 'pending',
       };
-      RESOURCES.set(key, cachedResource);
+
+      throw cacheRef.current.value;
     }
 
-    switch (cachedResource.state) {
-      case 'failure':
-        throw cachedResource.value;
-      case 'success':
-        return cachedResource.value;
+    switch (cacheRef.current.state) {
       case 'pending':
-        throw cachedResource.value;
+      case 'failure':
+        throw cacheRef.current.value;
+      case 'success':
+        return cacheRef.current.value;
       default:
         return <></>;
     }
   }
 
-  function Suspended(props: P): JSX.Element {
-    return load(
-      keySupplier(props),
-      () => render(props),
-    );
-  }
+  const Suspended = React.forwardRef<AsyncState | null, P>(
+    (props, ref) => (
+      load(
+        ref,
+        (subscription) => render(props, subscription),
+        dependencies(props),
+      )
+    ),
+  );
 
   function Base({ fallback, ...props }: P & AsyncFCProps): JSX.Element {
+    const cacheRef = React.useRef<AsyncState | null>(null);
+
+    useIsomorphicEffect(() => (): void => {
+      if (cacheRef.current?.state === 'pending') {
+        cacheRef.current.subscription.cancel();
+      }
+    }, []);
+
     if (typeof window === 'undefined') {
       return (
         <>{ fallback ?? defaultFallback }</>
       );
     }
+
     return (
       <Suspense fallback={<>{ fallback ?? defaultFallback }</>}>
-        <Suspended {...(props as P)} />
+        <Suspended ref={cacheRef} {...(props as React.PropsWithoutRef<P>)} />
       </Suspense>
     );
   }

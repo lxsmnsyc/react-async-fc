ed42bab2e52adf0d3e7bae31d68e178ff7581222
Alexis Munsayac <alexis.munsayac@gmail.com>
2020-07-26 22:15:42 +0800

    Update dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -25,7 +25,7 @@
  * @author Alexis Munsayac <alexis.munsayac@gmail.com>
  * @copyright Alexis Munsayac 2020
  */
-import * as React from 'react';
+import React from 'react';
 import createSuspenseful from './create-suspenseful';
 import createNonSuspense from './create-non-suspense';
 import { AsyncFunctionComponent, AsyncFCOptions, AsyncFCProps } from './types';
